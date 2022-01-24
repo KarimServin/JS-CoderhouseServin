43715115b96fb6950d95e31a2823da2b57edc940
Karim Jair Servin <93849158+KarimServin@users.noreply.github.com>
2022-01-24 13:58:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,85 @@
 
-let pregunta0=prompt("Que edad tiene?")
+alert("Bienvenido al simulador interactivo para calcular los costos de mi trabajo")
 
-if(pregunta0<18) {
-    alert("No trabajo con niños")
-} else {
+while(true) {
 
-let pregunta=prompt("Para que empresa trabaja?")
+    let canth=parseInt(prompt("Ingrese la cantidad de horas de trabajo"))
 
-while(pregunta=="") {
-     pregunta=prompt("POR FAVOR, DIGAME PARA QUE EMPRESA TRABAJA?")
+    if (canth!="") {
+
+        break;
+    }
+    else{
+
+        alert("No puso la cantidad de horas de trabajo")
+        continue;
+    }
+
 }
 
-switch(pregunta){
+let costo=parseInt(calcularCosto(canth));
 
-    case "Google":
-        alert("Cualquier trabajo que por el que ud. me contacte le va a costar 100 millones de dolares");
-        break;
-    case "Facebook":
-        alert("Cualquier trabajo que por el que ud. me contacte le va a costar 50 millones de dolares");
-        break;
-    case "Amazon":
-        alert("Cualquier trabajo que por el que ud. me contacte le va a costar 30 millones de dolares");
-        break;
-    case "Apple":
-        alert("Cualquier trabajo que por el que ud. me contacte le va a costar 50 millones de dolares");
-        break;    
-    case "Apple":
-        alert("Cualquier trabajo que por el que ud. me contacte le va a costar 50 millones de dolares");
-        break; 
-    default: 
-        alert("No trabajo con empresas pequeñas. Debo mantener mi reputación.")        
+alert("hola");
+alert("El costo en base a las horas de trabajo es" + costo + "dolares")
+
+let fp=formaPago();
+
+let costoRecargo=0;
+
+switch(fp) {
+
+    case 1: alert("Debe pagar impuestos"); costoRecargo=calcularImpuestos(costo); break;
+    case 2: alert("Debe pagarme en criptomonedas con un costo de comision"); costoRecargo=calcularComision(costo); break;
+    default: alert("No me contrate como programador, no le conviene"); break;
 }
+
+let costoTotal= costo + costoRecargo;
+
+alert("El costo total es" + costoTotal + "dolares, de los cuales" + costoRecargo + "corresponden a impuesto o comision")
+//////////////////
+
+function calcularCosto(costo) {
+
+    return costo*25;
+
 }
+
+function calcularImpuestos(costo) {
+
+    return costo*0.65; 
+
+
+}
+
+function calcularComision() {
+
+    return costo*0.1;
+
+}
+
+
+function formaPago() {
+
+    alert("hola");
+
+    while(true) {
+
+        let fp=parseInt(prompt("Como me va a pagar? 1- En blanco, 2- En negro"));
+
+
+    
+        if (fp!="") {
+    
+            break;
+        }
+        else{
+    
+            alert("No puso como me va a pagar")
+            continue;
+        }
+    
+    }
+
+    return fp;
+}
+
