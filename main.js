c8454a8bfe6f5407ca490e0a1376e6d205fdacfe
Karim Jair Servin <93849158+KarimServin@users.noreply.github.com>
2022-02-21 23:36:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,177 +1,134 @@
 
-//DECLARACION DE VARIABLES: DOM
-let bienvenido = document.getElementById("bienvenida");
-let input= document.getElementById('input1');
-let botoncito= document.getElementById('botoncito');
-
-//EVENTO DE TIPO ON CLICK
-botoncito.onclick = () =>{extraer()}
-
-
-//FUNCION DE EVENTO: extraer() VA A EXTRAER EL DATO INTRODUCIDO EN EL INPUT
-function extraer()
-{
-let dato = document.getElementById('input1').value;
-
-bienvenido.remove();
-input.remove();
-botoncito.remove();
-
- 
-//SE LE PASA A LA FUNCION debienvenida(dato) EL DATO PARA QUE ESTA LO PUEDA MANIPULAR.
-return debienvenida(dato);
-
-
-}
-
-
-
-
-
-
-//FUNCION QUE DARA LA BIENVENIDA DEL USUARIO, UTILIZANDO SU NOMBRE, Y LUEGO PERMITE BUSCAR LIBRO.
-function debienvenida(nombre) {
-
-let bienvenida= document.createElement("h5");
-
-bienvenida.innerHTML = `Te damos la bienvenida, ${nombre}. Muchas gracias por visitar nuestro nuevo e-commerce. `;
-
-document.body.append(bienvenida);
-
-let consulta = document.createElement("input");
-consulta.type = "text";
-consulta.className = 'input1'; 
-consulta.name = 'ingresotitulo';
-consulta.placeholder = "Ingrese en este lugar el titulo que desea consultar";
-consulta.size= 100;
-document.body.append(consulta);
-
-
-
-let btn = document.createElement("button");
-btn.innerHTML = "BUSCAR TITULO";
-document.body.appendChild(btn); //(REVISAR APPENDCHILD)
-btn.id='botoncito'; 
-
-btn.onclick = () =>{buscar(document.getElementsByClassName('input1')[0].value)} //LA FUNCION BUSCAR SE ENCUENTRA AL FINAL DEL JS
-
-}
-
-
-
-
-
-
- 
-// AQUI VIENE LA PARTE DE OBJETOS ///////////////////////////////////////////////////////////
 // CLASE LIBRO
 class Libro {
 //FUNCION CONSTRUCTORA
-    constructor(autor,titulo,precio,stock,origen) {
+    constructor(autor,titulo,precio,stock,origen,genero,img) {
         this.autor = autor.toUpperCase();
-        this.titulo = titulo.toUpperCase();
+        this.titulo = titulo
         this.precio = parseInt(precio);
         this.stock = parseInt(stock);
         this.origen = origen.toUpperCase();
+        this.genero = genero;
+        this.img = img;
     }
 
-//// FUNCION SUMAR IMPUESTO
-    sumarImpuesto() { 
-        if (this.origen==='ARGENTINA') {
-           //EL PRODUCTO ES NACIONAL Y SE LE APLICA SOLAMENTE IVA
-          this.precio=this.precio * 1.21;
 
-        } else {
+}
 
-            //AL PRODUCTO SE LE APLICA EL IVA + UN IMPUESTO DE 65% POR SER IMPORTADO
+// CREACION DE OBJETOS LIBRO
 
-            this.precio=this.precio * 1.65 + precio*0.21;
+let libro1 = new Libro('GEORGE ORWELL','1984',980,10,'ARGENTINA','NOVELA','...');
+let libro2 = new Libro('ADAM SMITH','La Riqueza de las Naciones',1590,10,'ARGENTINA','ECONOMIA','...');
+let libro3 = new Libro('VARGAS LLOSA','La ciudad y los perros',1350,5,'PERU','NOVELA','...');
+let libro4 = new Libro('STEWART','Calculo en una Variable',5390,3,'MEXICO','TECNICOS','...');
+let libro5 = new Libro('STEWART','Calculo en Varias Variables',5390,2,'MEXICO','TECNICOS','...');
+let libro6 = new Libro('SOMMERVILLE','Ingeneria en Software',4390,2,'MEXICO','TECNICOS','...');
+let libro7 = new Libro('Serway','Fisica I',5390,2,'MEXICO','TECNICOS','...');
+let libro8 = new Libro('Serway','Fisica II',5390,2,'MEXICO','TECNICOS','...');
+let libro9 = new Libro('Sears Zemansky','Fisica para Ingenieros',5390,2,'MEXICO','TECNICOS','...');
+let libro10 = new Libro('JAVIER MILEI','Desenmascarando la Mentira Keynesiana',2000,2,'ARGENTINA','ECONOMIA','...');
+let libro11 = new Libro('Eduardo Galeano','Las Venas Abiertas de America Latina',1000,2,'ARGENTINA','POLITICA','...');
+let libro12 = new Libro('Hayek','La Fatal Arrogancia',3000,2,'ARGENTINA','ECONOMIA','...');
+let libro13 = new Libro('Dante Aligheri','La Divina Comedia',955,4,'ARGENTINA','CLASICOS','...');
+let libro14 = new Libro('PLATON','La Republica',955,5,'ARGENTINA','CLASICOS','...');
+let libro15 = new Libro('JK Rowling','Harry Potter y el Prisionero de Azkaban',2340,5,'BRASIL','NOVELA','...');
+let libro16 = new Libro('Antonio de Saint-Exupery','El Principito',990,10,'ARGENTINA','NOVELA','...');
 
-        }
+//ARRAY CON TODOS LOS LIBROS
+
+let libros = [libro1,libro2,libro3,libro4,libro5,libro6,libro7,libro8,libro9,libro10,libro11,libro12,libro13,libro14,libro15,libro16];
+
+//SE DECLARA CARRITO COMO ABSTRACCION DE CARRITO DE COMPRAS: VA A SER UN ARRAY QUE EMPIECE VACIO. 
+let carrito=[];
+
+
+
+//SE OBTIENE EL CONTENEDOR DE PRODUCTOS DE CATALOGO.HTML
+const contenedorProductos = document.getElementById('contenedor-productos');
+const itemscarrito = document.getElementById('itemscarrito');
+
+desplegarCatalogo(libros);
+
+
+
+// CATALOGO: ITERACION OBJETOS, VA A MOSTRAR LAS CARDS CON LOS PRODUCTOS
+
+
+function desplegarCatalogo(array) {
+    contenedorProductos.innerHTML =''; //SE ASEGURA QUE EL CONTENEDOR EMPIECE SIN NADA
+
+for (const libro of array) { //LOS OBJETOS QUE SE RECORREN DEL ARRAY DE LIBROS SON OBJETOS LLAMADOS LIBRO
+    let div = document.createElement('div'); //SE CREA EL APARTADO DE ETIQUETA DIV 
+    div.className = 'producto';
+    div.innerHTML += `<div class="card m-3" style="width: 25rem">
+    <img src="${libro.img}" class="card-img-top" alt="imagen">
+    <div class="card-body">
+      <h5 class="card-title text-dark">${libro.titulo}</h5>
+      <p class="card-title text-dark">${libro.autor}</p>
+      
+      <p class="car-text text-dark">$ ${libro.precio}</p>
+      <a href="#" id="botonAgregar${libro.titulo}" class="btn btn-dark">Comprar</a>
+    </div>
+  </div>`
+                    
+    contenedorProductos.appendChild(div);
+
     
 
-          return this.precio;
-    
-        }
 
-//// FUNCION VENDER: REDUCE EL STOCK EN 1
+    let btnAgregar = document.getElementById(`botonAgregar${libro.titulo}`)
+// console.log(btnAgregar)
 
-    vender() {
+btnAgregar.addEventListener('click',()=>{
 
-        if(this.stock>0)  {
-
-    this.stock=this.stock - 1;
-
-    return(`Stock actual luego de vender: ${this.stock}`);
-
-  
-    } else {
-
-      return("no se puede vender. No disponemos de stock");
-
-    }
-
+    agregarCarrito(libro) 
+})
 }
 
 }
 
-// CREACION DE OBJETOS Y ARRAY DE OBJETOS
 
-let libro1 = new Libro('GEORGE ORWELL','1984',980,10,'ARGENTINA');
-let libro2 = new Libro('ADAM SMITH','La riqueza de las naciones',1590,10,'ARGENTINA');
-let libro3 = new Libro('VARGAS LLOSA','La ciudad y los perros',1350,5,'PERU');
-let libro4 = new Libro('STEWART','Calculo en varias variables',5390,2,'MEXICO');
+function agregarCarrito(libro) {
 
 
-let libros = [libro1,libro2,libro3,libro4];
+    carrito.push(libro)
+    let cantidad=1;
+    let div = document.createElement('tr'); //SE CREA EL APARTADO DE ETIQUETA tr 
+    div.className = 'producto';
+    div.innerHTML += `<tr>
+    <th scope="row">${cantidad}</th>
+    <td>${libro.titulo}</td>
+    <td>${libro.autor}</td>
+    <td>$ ${libro.precio}</td>
+  </tr>`
+                    
+    itemscarrito.appendChild(div);
+
+    
 
 
-// FUNCION BUSCAR
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
 
-function buscar(title) {
-
-title=title.toUpperCase();
-
-let pr=0;
-let st=0;
-
-    //RECORRE EL ARRAY DE INSTANCIAS DECLARADO ANTERIORMENTE
-for(const libro of libros) {
-  
- 
-  if (title==libro.titulo) {
-  //SI COINCIDE CON ALGUN MODELO EN EL SISTEMA, PR VA A ALMACENAR EL PRECIO CORRESPONDIENTE, Y ST EL STOCK.
-      pr=libro.precio;
-      st=libro.stock;
-      break;
-     }
-  
-  }
-  
-  
-
-  if(pr !==0 && st!==0) {
-  
-  
- 
-    let info= document.createElement("h5");
-   info.innerHTML = `El libro que usted consultó, ${title}, tiene un valor de $${pr} y su stock actual es de ${st}`;
-   document.body.append(info);
- 
-  
-  
-  } else {
-  
-    let info= document.createElement("h5");
-    info.innerHTML = `No hemos podido encontrar el modelo que usted consultó o no disponemos de stock`;
-   document.body.append(info);
-   
-  
-  //COMO NO PUDO ENCONTRAR EL MODELO, PR Y ST SIGUEN EN VALOR 0 COMO SE INICIALIZARON.
-  
-  }
-  } 
+localStorage.setItem('carrito', JSON.stringify(carrito));
 
 
 
+//ENTREGA OPERADORES AVANZADOS
+const financiacionCredito = [3,6,9];
+const combined = [1,2,4, ...financiacionCredito,12];
+console.log('Usted puede pagar  en ' + combined + ' cuotas sin interes');
 
+function reportarFaltaStock(libros) {
+
+  for(const libro of libros) {
+
+if (libro.stock==0 || precio==0) {
+
+  console.log(`Debemos verificar este articulo:  ${libro.titulo}` );
+
+}
+
+    }
+
+}
